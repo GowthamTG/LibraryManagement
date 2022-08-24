029d8964079b9f8da0e0f55af681211e3ce11c4a
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 00:11:58 +0530

    Form Added

--- Library-UI/src/app/modules/books/books.module.ts
+++ Library-UI/src/app/modules/books/books.module.ts
@@ -4,11 +4,22 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from '../books-routing.module';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AddBookComponent } from './add-book/add-book.component';
 import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [AddBookComponent, LandingComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
 })
 export class BooksModule {}
--- Library-UI/src/app/modules/books/landing/landing.component.ts
+++ Library-UI/src/app/modules/books/landing/landing.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
+  numbers = Array(10)
+    .fill(0)
+    .map((x, i) => i);
 
-  constructor() { }
+  filterForm: FormGroup = this.fb.group({
+    name: [''],
+    author: [''],
+    subject: [''],
+    dateFrom: [''],
+    dateTo: [''],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

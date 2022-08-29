24fb52b0598f4b0882b7a59a7052516bfd524eb2
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-29 20:20:24 +0530

    Spinner Added

--- Library-UI/src/app/services/api-client.service.ts
+++ Library-UI/src/app/services/api-client.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ApiClientService {
-  // host: string = "https://library-backend-devrev.herokuapp.com";
-  host: string = "http://localhost:5000";
+  host: string = 'https://library-backend-devrev.herokuapp.com';
+  // host: string = "http://localhost:5000";
 
   constructor(private http: HttpClient) {}
 
   throwError(err: { error: { errorMessage: string } }): string {
     let message: string = err.error.errorMessage;
-    throw message || "Please try later";
+    throw message || 'Please try later';
   }
 
   getBooksCondition(filterDetails: any): Observable<any> {
